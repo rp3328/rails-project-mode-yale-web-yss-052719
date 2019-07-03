a5b0238c501eac92810856a440045e56d9a3d89f
github username <github email address>
2019-07-03 13:23:42 -0400

    user edits debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,8 @@ class UsersController < ApplicationController
 
     def edit
       @user = User.find(params[:id])
-  end
+      @types = Type.all
+    end
 
   def update
       @user = User.find(params[:id])
