7a5ae6643c4315c4a7bdc4f334a0b11108783943
github username <github email address>
2019-07-04 15:46:21 -0400

    background edits

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module RailsProjectModeYaleWebYss052719
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.serve_static_assets = true
-    # config.serve_static_assets = true
     config.assets.compile = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
